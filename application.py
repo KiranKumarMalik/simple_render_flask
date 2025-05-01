2d99816fb3df90ffc3395a1b39c2cacf3a5b6d70
KiranKumarMalik <malikkiran413@gmail.com>
2025-05-01 11:44:04 +0530

    new flask practice

---
+++ application.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+FAI = Flask(__name__)
+
+@FAI.route('/index')
+def index():
+    return "Hello, World!"
+
+@FAI.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    FAI.run(debug=True)
